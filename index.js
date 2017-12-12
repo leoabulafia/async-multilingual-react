0a890c229d195dc58732ae1b25a6eeb6aa6a10dd
leoabulafia <ldavalos.mail@gmail.com>
2017-12-12 14:02:23 +0200

    modify scripts

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const app = express();
 const path = require('path');
 const content = require('./api/content');
 
-app.use('/api', express.static('public'));
-
 app.get('/api/lang', (req, res) => {
   res.send(req.headers['accept-language'].split(',')[0].slice(0, 2));
 });
